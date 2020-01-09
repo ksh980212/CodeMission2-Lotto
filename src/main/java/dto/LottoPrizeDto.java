3da84f1c2a69ddb54638831eacdb6857d03ec988
ksh980212 <ksh980212@gmail.com>
2020-01-09 22:04:41 +0900

    로또 Prize domain 구현

--- src/main/java/dto/LottoPrizeDto.java
+++ src/main/java/dto/LottoPrizeDto.java
@@ -10,7 +10,7 @@ public class LottoPrizeDto {
   private List<LottoPrize> lottoPrizeList;
 
   private LottoPrizeDto(List<Lotto> lottoList) {
-    lottoList = new ArrayList<>();
+    lottoPrizeList = new ArrayList<>();
     for(int i = 0, end= lottoList.size() ; i < end ; i++ ) {
       lottoPrizeList.add(lottoList.get(i).getPrize());
     }
@@ -23,5 +23,7 @@ public class LottoPrizeDto {
   public void showPrizeStatus() {
     System.out.println("Winning statistics");
     System.out.println("-------------------");
+
+    //생각해 봅시다 어떻게 보여주면 좋을지
   }
 }
