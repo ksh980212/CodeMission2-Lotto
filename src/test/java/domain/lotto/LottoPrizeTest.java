4bbd45380de5839fcb73885a2e924a84f2f2527f
ksh980212 <ksh980212@gmail.com>
2020-01-10 13:25:32 +0900

    로또 테스트 코드 추가

---
+++ src/test/java/domain/lotto/LottoPrizeTest.java
@@ -0,0 +1,32 @@
+package domain.lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class LottoPrizeTest {
+
+  @Test
+  void 여섯개를_맞추면_1등이_된다() {
+    //given
+    long matchedCount = 6;
+
+    //when
+    LottoPrize prize = LottoPrize.of(matchedCount);
+
+    //then
+    assertThat(prize).isEqualTo(LottoPrize.FIRST);
+  }
+
+  @Test
+  void 하나도_맞추지못하면_NONE_이된다() {
+    //given
+    long matchedCount = 0;
+
+    //when
+    LottoPrize prize = LottoPrize.of(matchedCount);
+
+    //then
+    assertThat(prize).isEqualTo(LottoPrize.NONE);
+  }
+}
